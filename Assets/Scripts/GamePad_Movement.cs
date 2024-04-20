ef8a7b2d6127cfa77067bb43626ed60fff44d23b
23Wolter <wolter.nielsen@gmail.com>
2024-04-20 22:27:19 +0200

    new gamepad controller

--- Assets/Scripts/GamePad_Movement.cs
+++ Assets/Scripts/GamePad_Movement.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class GamePad_Movement : MonoBehaviour
 {
-    private float speed = .1f;
+    public float speed = .1f;
     private float smoothTime = 0.05f;
     private float CurrentVelo;
+    private CharacterController controller;
+    private float gravityValue = -40f;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+
+    void Start() 
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
     void Update()
     {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
         float xDir = Input.GetAxis("LeftJoystickX");
         float yDir = Input.GetAxis("LeftJoystickY");
+
+        Vector3 move = new Vector3(xDir, 0, yDir);
+        controller.Move(move * Time.deltaTime * speed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
 
         var TargetAngle = Mathf.Atan2(xDir, yDir) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref CurrentVelo, smoothTime);
         transform.rotation = Quaternion.Euler(0.0f, TargetAngle, 0.0f);
 
-        Vector3 MoveDir = new Vector3(xDir, 0.0f, yDir);
+        // Vector3 MoveDir = new Vector3(xDir, 0.0f, yDir);
 
-        transform.position += MoveDir * speed;
-
+        // transform.position += MoveDir * speed;
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
 
     }
 }
