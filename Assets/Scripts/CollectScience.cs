e464a32738acd26f978a2f5df41c786ca0eab7d3
KingShake <62018328+KingShake@users.noreply.github.com>
2024-04-20 22:22:01 +0200

    YOu can noow collect stuff and a small animation

---
+++ Assets/Scripts/CollectScience.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectScience : MonoBehaviour
+{
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        //var points = 0;
+        var playerType = 0;
+
+        if (other.tag == "Point")
+        {
+            if (this.gameObject.tag == "Player1")
+            {
+                playerType = 1;
+
+            }
+            if (this.gameObject.tag == "Player2")
+            {
+                playerType = 2;
+            }
+            if (this.gameObject.tag == "Player3")
+            {
+                playerType = 3;
+            }
+        }
+        scoreManeger.Instance.AddPoint(playerType);
+        Destroy(other.gameObject);
+    }
+}
